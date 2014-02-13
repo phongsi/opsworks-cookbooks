201c13b996d5c1b922093659357305c2fa153cf4
Phong Si <phong.si@gmail.com>
2014-02-12 20:56:12 -0500

    try adding sswap space

---
+++ swap/recipes/default.rb
@@ -0,0 +1,7 @@
+execute "Adding 1gb of swap space" do
+  command <<-CMD
+    /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
+    /sbin/mkswap /var/swap.1
+    /sbin/swapon /var/swap.1
+  CMD
+end
