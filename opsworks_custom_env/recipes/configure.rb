080407af26f7068871514c2641adc7b078d5d04d
Phong Si <phong.si@gmail.com>
2014-02-14 18:49:17 -0500

    run custom env on delayed job workers layer

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -3,4 +3,8 @@ if node[:opsworks][:instance][:layers].include?('rails-app')
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
 
+elsif node[:opsworks][:instance][:layers].include?('delayed-job-workers')
+
+  include_recipe "opsworks_custom_env::write_config"
+
 end
