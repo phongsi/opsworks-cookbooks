66ddf3eea117b8f86c62e36c5496b888d327dc35
Phong Si <phong.si@gmail.com>
2014-02-18 21:29:58 -0500

    run custom env for scheduled jobs layer

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -1,4 +1,4 @@
-if node[:opsworks][:instance][:layers].include?('rails-app') || node[:opsworks][:instance][:layers].include?('delayed-job-workers')
+if node[:opsworks][:instance][:layers].include?('rails-app') || node[:opsworks][:instance][:layers].include?('delayed-job-workers') || node[:opsworks][:instance][:layers].include?('scheduled_jobs')
 
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
