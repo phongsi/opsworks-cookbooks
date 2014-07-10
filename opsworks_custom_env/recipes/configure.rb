4c13f44668d4e95ef9ad87465e087e2dbd6bbcfb
Phong Si <phong.si@gmail.com>
2014-07-10 12:32:53 -0400

    test output

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -1,5 +1,5 @@
 if node[:opsworks][:instance][:layers].include?('rails-app') || node[:opsworks][:instance][:layers].include?('delayed-job-workers') || node[:opsworks][:instance][:layers].include?('scheduled-jobs')
-
+  puts 'testing----------'
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
 
