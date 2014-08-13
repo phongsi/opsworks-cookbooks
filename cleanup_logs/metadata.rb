b43f8554cee6c29fc0acc73ab94b1bed8ab73e9b
Phong Si <phong.si@gmail.com>
2014-08-13 12:03:00 -0400

    add cleanup recipe

---
+++ cleanup_logs/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Phong Si"
+maintainer_email "phong.si@gmail.com"
+license          "MIT"
+description      "Configure and cron to cleanup old logs"
+version          "0.1"
+
+recipe 'cleanup_logs::default', 'Sets up cron on new and running instances.'
+
+depends 'deploy'
