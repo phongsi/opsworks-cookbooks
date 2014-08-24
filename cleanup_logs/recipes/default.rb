a10933398e5d9fb1852dbda718c5a56230a581b4
Phong Si <phong.si@gmail.com>
2014-08-24 10:12:39 -0400

    run rm logs every night

--- cleanup_logs/recipes/default.rb
+++ cleanup_logs/recipes/default.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |application, deploy|
   cron "clean_logs" do
     minute "0"
     hour "1"
-    weekday "6"
+    weekday "*"
     command "find /srv/www/#{application}/shared/log -mtime +7 -type f -name \"*.log*\" | xargs rm"
   end
 end
