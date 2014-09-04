a7a4eaf148bce8682e8cbb27844c36c6246b3bc5
Phong Si <phong.si@gmail.com>
2014-09-04 16:04:28 -0400

    try including path and user dpeloy

--- clockwise_cron_tasks/recipes/default.rb
+++ clockwise_cron_tasks/recipes/default.rb
@@ -1,12 +1,12 @@
 # 9am utc is 5am eastern
 node[:deploy].each do |application, deploy|
+  cron_env = {"PATH" => "/usr/local/bin:$PATH"}
   cron "send_daily_report" do
+    environment cron_env
+    user "deploy"
     minute "*/10"
     hour "*"
     weekday "*"
-    command %Q{
-      cd /srv/www/#{application}/current &&
-      /usr/bin/env /usr/local/bin/ruby script/rails runner -e production "ActionAutomator.send_morning_stat_reports"
-    }
+    command "cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner 'ActionAutomator.send_morning_stat_reports'"
   end
 end
