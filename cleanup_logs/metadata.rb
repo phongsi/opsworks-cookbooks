8b2e51b69d06d16cbca327e60886483506c6f645
Phong Si <phong.si@gmail.com>
2014-09-04 15:25:42 -0400

    try cron fro sending report

--- cleanup_logs/metadata.rb
+++ cleanup_logs/metadata.rb
@@ -4,6 +4,6 @@ license          "MIT"
 description      "Configure and cron to cleanup old logs"
 version          "0.1"
 
-recipe 'cleanup_logs::default', 'Sets up cron on new and running instances.'
+recipe 'cleanup_logs::default', 'Sets up cron on new and running instances. Use on setup and deploy.'
 
 depends 'deploy'
---
+++ clockwise_cron_tasks/recipes/default.rb
@@ -0,0 +1,12 @@
+# 9am utc is 5am eastern
+node[:deploy].each do |application, deploy|
+  cron "send_daily_report" do
+    minute "*/10"
+    hour "*"
+    weekday "*"
+    command %Q{
+      cd /srv/www/#{application}/current
+      /usr/bin/env /usr/local/bin/ruby bin/rails runner -e production "ActionAutomator.send_morning_stat_reports"
+    }
+  end
+end
