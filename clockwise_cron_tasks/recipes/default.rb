c89a86ad5fb7bf4e8a4cfad26d985439c5aa8a30
Phong Si <phong.si@gmail.com>
2014-09-04 16:18:46 -0400

    change time

--- clockwise_cron_tasks/recipes/default.rb
+++ clockwise_cron_tasks/recipes/default.rb
@@ -3,10 +3,9 @@ node[:deploy].each do |application, deploy|
   cron_env = {"PATH" => "/usr/local/bin:$PATH"}
   cron "send_daily_report" do
     environment cron_env
-    user "deploy"
-    minute "*/10"
-    hour "*"
+    minute "0"
+    hour "9"
     weekday "*"
-    command "cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner 'ActionAutomator.send_morning_stat_reports'"
+    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \'ActionAutomator.send_morning_stat_reports\''"
   end
 end
