7372e8bf65c457e3d651d9ac5ab030c2f5264160
Phong Si <phong.si@gmail.com>
2014-09-04 16:25:07 -0400

    fix syntax

--- clockwise_cron_tasks/recipes/default.rb
+++ clockwise_cron_tasks/recipes/default.rb
@@ -6,6 +6,6 @@ node[:deploy].each do |application, deploy|
     minute "0"
     hour "9"
     weekday "*"
-    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \'ActionAutomator.send_morning_stat_reports\''"
+    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.send_morning_stat_reports\"'"
   end
 end
