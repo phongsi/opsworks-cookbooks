d17704c2432526201219970d92aa76c03afc7124
Dan DeBruler <ddebruler@gmail.com>
2014-10-22 15:51:19 -0400

    add chron tasks specific to metrics application

---
+++ clockwise_metrics_chron_tasks/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Dan DeBruler"
+maintainer_email "ddebruler@gmail.com"
+license          "MIT"
+description      "Configure and cron to run Clockwise specific tasks"
+version          "0.1"
+
+recipe 'clockwise_metrics_chron_tasks::default', 'Sets up cron on running instances. Use on deploy.'
+
+depends 'deploy'
