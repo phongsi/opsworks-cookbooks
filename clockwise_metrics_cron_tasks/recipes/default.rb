5edba23b73017c5dc21d6b80c24dfe358bd4488f
Dan DeBruler <ddebruler@gmail.com>
2014-10-22 21:36:52 -0400

    correct wildcard in cron time flag

--- clockwise_metrics_cron_tasks/recipes/default.rb
+++ clockwise_metrics_cron_tasks/recipes/default.rb
@@ -3,14 +3,14 @@ node[:deploy].each do |application, deploy|
   cron_env = {"PATH" => "/usr/local/bin:$PATH"}
   cron "check_wait_rooms" do
     environment cron_env
-    minute "0/2"
+    minute "*/2"
     hour "*"
     weekday "*"
     command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.check_wait_rooms\"'"
   end
   cron "check_ontime_status" do
     environment cron_env
-    minute "0/2"
+    minute "*/2"
     hour "*"
     weekday "*"
     command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.check_ontime_status\"'"
