2462699a9ee314c80ad58b3232d106050e862ab3
Dan DeBruler <ddebruler@gmail.com>
2014-10-22 20:58:00 -0400

    fix typo

--- clockwise_metrics_cron_tasks/metadata.rb
+++ clockwise_metrics_cron_tasks/metadata.rb
@@ -4,6 +4,6 @@ license          "MIT"
 description      "Configure and cron to run Clockwise specific tasks"
 version          "0.1"
 
-recipe 'clockwise_metrics_chron_tasks::default', 'Sets up cron on running instances. Use on deploy.'
+recipe 'clockwise_metrics_cron_tasks::default', 'Sets up cron on running instances. Use on deploy.'
 
 depends 'deploy'
