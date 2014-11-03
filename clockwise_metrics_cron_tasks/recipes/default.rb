f4b5b401df058f78f975188b642dc847bd618fdb
Dan DeBruler <ddebruler@gmail.com>
2014-11-03 15:13:09 -0500

    add discharge and missing times to metrics scheduled tasks

--- clockwise_metrics_cron_tasks/recipes/default.rb
+++ clockwise_metrics_cron_tasks/recipes/default.rb
@@ -15,4 +15,18 @@ node[:deploy].each do |application, deploy|
     weekday "*"
     command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.check_ontime_status\"'"
   end
+  cron "discharge_old_patients" do
+    environment cron_env
+    minute "0"
+    hour "9"
+    weekday "*"
+    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.discharge_appointments\"'"
+  end
+  cron "fill_missing_timestamps" do
+    environment cron_env
+    minute "0"
+    hour "9"
+    weekday "*"
+    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.request_timestamps\"'"
+  end
 end
