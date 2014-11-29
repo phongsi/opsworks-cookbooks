202f6634d004a213203165ea56a75288c28a1204
Phong Si <phong.si@gmail.com>
2014-11-29 02:39:44 -0500

    add clean stale apts and clean oauth to cron

--- clockwise_cron_tasks/recipes/default.rb
+++ clockwise_cron_tasks/recipes/default.rb
@@ -31,3 +31,25 @@ node[:deploy].each do |application, deploy|
     command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"ActionAutomator.send_daily_group_performance_reports\"'"
   end
 end
+
+node[:deploy].each do |application, deploy|
+  cron_env = {"PATH" => "/usr/local/bin:$PATH"}
+  cron "cleanup_oauth" do
+    environment cron_env
+    minute "0"
+    hour "9"
+    weekday "*"
+    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"OauthCleaner.clean_grants;OauthCleaner.clean_tokens\"'"
+  end
+end
+
+node[:deploy].each do |application, deploy|
+  cron_env = {"PATH" => "/usr/local/bin:$PATH"}
+  cron "cleanup_stale_apts" do
+    environment cron_env
+    minute "0"
+    hour "*"
+    weekday "*"
+    command "/bin/su - #{deploy[:user]} -c 'cd /srv/www/#{application}/current && RAILS_ENV=production bundle exec rails runner \"Cleaner.clean\"'"
+  end
+end
