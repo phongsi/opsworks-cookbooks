111793b1eb99a412264a5e20ce4b5fa7f0be889f
Phong Si <phong.si@gmail.com>
2014-12-03 13:30:32 -0500

    clean nginx logs if any

--- cleanup_logs/recipes/default.rb
+++ cleanup_logs/recipes/default.rb
@@ -6,4 +6,12 @@ node[:deploy].each do |application, deploy|
     weekday "*"
     command "find /srv/www/#{application}/shared/log -mtime +1 -type f -name \"*.log*\" | xargs rm"
   end
+
+  cron "clean_nginx_logs" do
+    minute "0"
+    hour "5"
+    weekday "*"
+    command "find /var/log/nginx -mtime +1 -type f -name \"*.log*\" | xargs rm"
+    only_if { ::File.directory?("/var/log/nginx") }
+  end
 end
