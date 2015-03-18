93961c06b99a379abc7c78996b6a0befe7992c30
Phong Si <phong.si@gmail.com>
2015-03-17 23:15:59 -0400

    update to clean logstash logs

--- cleanup_logs/recipes/default.rb
+++ cleanup_logs/recipes/default.rb
@@ -14,4 +14,12 @@ node[:deploy].each do |application, deploy|
     command "find /var/log/nginx -mtime +1 -type f -name \"*.log*\" | xargs rm"
     only_if { ::File.directory?("/var/log/nginx") }
   end
+
+  cron "clean_logstash_logs" do
+    minute "0"
+    hour "5"
+    weekday "*"
+    command "find /var/log/logstash -mtime +1 -type f -name \"*.log*\" | xargs rm"
+    only_if { ::File.directory?("/var/log/logstash") }
+  end
 end
