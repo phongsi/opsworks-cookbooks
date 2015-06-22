176fc51d0fc31245f6c4da2207c5d4fcd766159c
Phong Si <phong.si@gmail.com>
2015-06-22 12:56:23 -0400

    rename var

--- insert_layer_types/recipes/default.rb
+++ insert_layer_types/recipes/default.rb
@@ -2,7 +2,7 @@ node[:deploy].each do |application, deploy|
   ruby_block "insert_test" do
     block do
       file = Chef::Util::FileEdit.new("#{deploy[:deploy_to]}/shared/config/application.yml")
-      file.insert_line_if_no_match("/layer-types/", "layer-types: #{node[:opsworks][:instance][:layers].join(',')}")
+      file.insert_line_if_no_match("/LAYER_TYPES/", "LAYER_TYPES: \"#{node[:opsworks][:instance][:layers].join(',')}\"")
       file.write_file
     end
   end
