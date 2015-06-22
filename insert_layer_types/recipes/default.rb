eb0170e35b941df67dd2c567e5c03b43a9e31661
Phong Si <phong.si@gmail.com>
2015-06-22 12:48:10 -0400

    update vars

--- insert_layer_types/recipes/default.rb
+++ insert_layer_types/recipes/default.rb
@@ -1,7 +1,9 @@
-ruby_block "insert_test" do
-  block do
-    file = Chef::Util::FileEdit.new("#{params[:deploy][:deploy_to]}/shared/config/application.yml")
-    file.insert_line_if_no_match("/layer-types/", "layer-types: #{node[:opsworks][:instance][:layers].join(',')}")
-    file.write_file
+node[:deploy].each do |application, deploy|
+  ruby_block "insert_test" do
+    block do
+      file = Chef::Util::FileEdit.new("#{deploy[:deploy_to]}/shared/config/application.yml")
+      file.insert_line_if_no_match("/layer-types/", "layer-types: #{node[:opsworks][:instance][:layers].join(',')}")
+      file.write_file
+    end
   end
 end
