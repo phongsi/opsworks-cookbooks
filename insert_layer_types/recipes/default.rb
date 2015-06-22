ad7e9ac2c1f579f106e9023b0fbf7d9665af30fd
Phong Si <phong.si@gmail.com>
2015-06-22 12:07:01 -0400

    add test recipe

--- insert_layer_types/recipes/default.rb
+++ insert_layer_types/recipes/default.rb
@@ -1,7 +1,7 @@
 ruby_block "insert_test" do
   block do
     file = Chef::Util::FileEdit.new("#{params[:deploy][:deploy_to]}/shared/config/application.yml")
-    file.insert_line_if_no_match("/layer-types/", "layer-types: #{node[:opsworks][:instance][:layers].join(', ')}")
+    file.insert_line_if_no_match("/layer-types/", "layer-types: #{node[:opsworks][:instance][:layers].join(',')}")
     file.write_file
   end
 end
