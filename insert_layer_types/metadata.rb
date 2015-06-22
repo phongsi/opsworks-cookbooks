8af29aa6628409540de03e2071097ee08ba21546
Phong Si <phong.si@gmail.com>
2015-06-22 12:24:31 -0400

    add test

---
+++ insert_layer_types/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Dean and Phong"
+maintainer_email "phong.si@gmail.com"
+license          "MIT"
+description      "Configure and deploy custom daemon"
+version          "0.1"
+
+recipe 'insert_layer_types::default', 'add to application.yml'
+
+depends 'deploy'
